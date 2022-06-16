6716142d234593882fa16c28aa3f79e9e045787c
Michal Parusinski <michal@parusinski.me>
2022-06-16 16:48:11 +0200

    feat: implementation of finding winner in tic tac toe

--- examples/tictactoe.hs
+++ examples/tictactoe.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Control.Monad
 
 data TTTEntry = Circle | Cross | Empty
     deriving (Show, Eq)
@@ -29,3 +30,18 @@ getForwardDiagonal st = zipWith (!!) st [0..]
 
 getBackwardsDiagonal :: TTTState -> TTTLine
 getBackwardsDiagonal st = zipWith (!!) (reverse st) [0..]
+
+isIdentical :: Eq a => [a] -> Bool
+isIdentical line = and $ map ((head line) ==) $ tail line
+
+winCondition :: TTTState -> Maybe TTTEntry
+winCondition st = msum [rowWin, colWin, fdWin, bdWin]
+    where rowWin = fmap (head . (rows !!)) $ findIndex isIdentical rows
+          colWin = fmap (head . (cols !!)) $ findIndex isIdentical cols
+          fdWin  = if isIdentical fdiag then Just $ head fdiag else Nothing
+          bdWin  = if isIdentical bdiag then Just $ head bdiag else Nothing
+          fdiag  = getForwardDiagonal st
+          bdiag  = getBackwardsDiagonal st
+          rows   = map (getRow st) [0..tl]
+          cols   = map (getCol st) [0..tl]
+          tl     = length st - 1
