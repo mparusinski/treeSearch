e950492108a069224170da7c82d10dd7cb4c325a
Michal Parusinski <michal@parusinski.me>
2022-06-18 11:42:34 +0200

    deploy: Creating a stack package

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for treeSearch
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Lib
+-- import Data.Tree
+
+-- This is a simple tree with a single root (also a leaf)
+-- and no subtrees
+-- simpleTree = Node {
+--     rootLabel = "Hello, world!",
+--     subForest = []
+-- }
+
+main :: IO ()
+main = do putStrLn "Hello, world!"
