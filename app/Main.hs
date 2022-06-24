a2ce4e14a2934a9fefde0d7c1aabc646cac17045
Michal Parusinski <michal@parusinski.me>
2022-06-24 11:17:49 +0200

    feat: adding data.tree to stack project

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,6 @@
 module Main where
 
 import Lib
--- import Data.Tree
-
--- This is a simple tree with a single root (also a leaf)
--- and no subtrees
--- simpleTree = Node {
---     rootLabel = "Hello, world!",
---     subForest = []
--- }
 
 main :: IO ()
-main = do putStrLn "Hello, world!"
+main = exampleFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
 module Lib
-    ( someFunc
+    ( exampleFunc
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Tree
+
+-- This is a simple tree with a single root (also a leaf)
+-- and no subtrees
+simpleTree = Node {
+    rootLabel = "Hello, world!",
+    subForest = []
+}
+
+exampleFunc :: IO ()
+exampleFunc = putStrLn $ drawTree simpleTree
